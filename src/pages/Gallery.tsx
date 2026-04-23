7231c433f063cbfb3002048477d10038b5ca2a90
Harsh <joshiharsh920@gmail.com>
2026-04-23 22:22:04 +0530

    Updates

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -16,8 +16,12 @@ const Gallery = () => {
     const images2 = [
         "/images/img5.jpg",
         "/images/img6.jpg",
-        "/images/img3.jpg",
-        "/images/img4.jpg",
+        "/images/img7.jpg",
+        "/images/img8.jpg",
+        "/images/img9.jpg",
+        "/images/img10.jpg",
+        "/images/img11.jpg",
+        "/images/img12.jpg",
     ];
 
 
